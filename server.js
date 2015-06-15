0219205b426a5b6e406ae899271d7b9f43e4c458
son_cativo <son_cativo@hotmail.com>
2015-06-16 00:10:03 +0200

    Primera ruta: post

--- server.js
+++ server.js
@@ -11,8 +11,22 @@ var port = process.env.PORT || 8000;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hola mundo');
+// Rutas
+app.post('/planetas', function (req, res) {
+
+  // manipulamos la request
+  var nuevoPlaneta = req.body.planeta;
+  nuevoPlaneta.id = '123';
+
+  //preparamos la respuesta
+  res.set('Content-Type','application/json');
+  res.status(201);
+
+  // Enviamos la respuesta
+  res.json({
+    planeta: nuevoPlaneta
+  });
+
 });
 
 app.listen(port, function () {
