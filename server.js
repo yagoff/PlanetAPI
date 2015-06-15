6e6ae916f431386732273813d43d93aaceaea6d8
son_cativo <son_cativo@hotmail.com>
2015-06-15 23:54:32 +0200

    Primeros middlewares: logger y body parser

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// Dependencias
 var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// Setup App
 var app = express();
+var port = process.env.PORT || 8000;
+
+// Middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hola mundo');
 });
 
-app.listen(8000, function () {
-  console.log('Listening on port 8000...');
+app.listen(port, function () {
+  console.log('Listening on port ' + port + '...');
 });
